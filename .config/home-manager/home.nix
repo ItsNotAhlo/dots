ed95729bc8c4b3745057cb101f6542368c10d081
Ahlo <alpha15@ahlosclub.com>
2024-06-11 06:21:29 +0930

    Initial Commit

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,180 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "ahlo";
+  home.homeDirectory = "/home/ahlo";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # hello
+    ani-cli
+    atlauncher
+    bitwarden-desktop
+    blender
+    brave
+    chatterino2
+    discordo
+    eza
+    flameshot
+    gimp
+    helvum
+    ivpn
+    lunarvim
+    modrinth-app
+    mpv
+    mumble
+    nerdfonts
+    obs-studio
+    obsidian
+    osu-lazer
+    pipewire
+    protonup-qt
+    qemu
+    qtemu
+    qutebrowser
+    slack
+    spotify
+    steam
+    stow
+    tailscale
+    texmaker
+    thunderbird
+    transmission
+    vlc
+    webcord-vencord
+    yt-dlp
+    zathura
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  
+#  wayland.windowManager.hyprland = {
+#    enable = true;
+#    xwayland.enable = true;
+#    systemd.enable = true;
+#    };
+#  
+#    wayland.windowManager.hyprland.settings = {
+#      decoration = {
+#        shadow_offset = "0 5";
+#        "col.shadow" = "rgba(00000099)";
+#      };
+#  
+#      "$mod" = "SUPER";
+#  
+#      bindm = [
+#        # mouse movements
+#        "$mod, mouse:272, movewindow"
+#        "$mod, mouse:273, resizewindow"
+#        "$mod ALT, mouse:272, resizewindow"
+#      ];
+#    };
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/ahlo/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  programs.git = {
+  enable = true;
+  userName = "Ahlo";
+  userEmail = "alpha15@ahlosclub.com";
+  };
+
+#  programs.zsh = {
+#  enable = true;
+#  enableCompletion = false;
+#  autosuggestion.enable = true;
+#  syntaxHighlighting.enable = true;
+#  antidote.enable = true;
+#
+#  antidote.plugins = [
+#  "hlissner/zsh-autopair"
+#  "marlonrichert/zsh-autocomplete"
+#  ];
+#
+#  shellAliases = {
+#    ls = "ls -a";
+#    update = "home-manager switch";
+#    nvim = "lvim";
+#    vim = "lvim";
+#    test = "home-manager build";
+#    ani-cli = "ani-cli-unwrapped --skip";
+#    };
+#  };
+
+  programs = {
+    zsh = (import ./zsh.nix { inherit config pkgs; });
+    tmux = (import ./tmux.nix { inherit pkgs; });
+    alacritty = (import ./alacritty.nix { inherit config pkgs; });
+    fzf = (import ./fzf.nix { inherit pkgs; });
+  };
+
+  wayland.windowManager = {
+    hyprland = (import ./hyprland.nix { inherit pkgs; });
+  };
+#  programs.neovim = {
+#      enable = true;
+#      plugins = with pkgs.vimPlugins; [
+#        nvchad
+#	nvchad-ui
+#	];
+#  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
---
+++ .config/home-manager/hyprland.nix
@@ -0,0 +1,80 @@
+{pkgs, ...}:
+{
+  enable = true;
+  settings = {
+    "$mod" = "SUPER";
+    input = {
+      monitor = [
+      ];
+    };
+    bind =
+    [
+      "$mod, Q, exec, alacritty"
+      "$mod, C, killactive"
+      "$mod, M, exit"
+      "$mod, F, exec, firefox"
+      "$mod, E, exec, nautilus"
+      "$mod, V, togglefloating"
+      "$mod, R, exec, wofi --show drun"
+      "$mod, P, pseudo"
+      "$mod, J, togglesplit"
+      "$mod, T, togglegroup"
+      "$mod+ALT, J, changegroupactive, f"
+      "$mod+ALT, K, changegroupactive, f"
+      "$mod, left, movefocus, l"
+      "$mod, right, movefocus, r"
+      "$mod, up, movefocus, u"
+      "$mod, down, movefocus, d"
+    ]
+    ++ (
+      builtins.concatLists (builtins.genList (
+        x: let
+          ws = let
+            c = (x + 1) / 10;
+          in
+            builtins.toString(x + 1 - (c * 10));
+        in [
+          "$mod, ${ws}, workspace, ${toString (x + 1)}"
+          "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+        ]
+      )
+      10)
+    );
+  };
+
+  extraConfig = ''
+  exec-once = ags
+  exec = gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"   # for GTK3 apps
+  exec = gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"   # for GTK4 apps
+
+  env = QT_QPA_PLATFORMTHEME,qt6ct   # for Qt apps
+
+  bezier=easeOutBack,0.34,1.56,0.64,1
+  bezier=easeInBack,0.36,0,0.66,-0.56
+  bezier=easeInCubic,0.32,0,0.67,0
+  bezier=easeInOutCubic,0.65,0,0.35,1
+  animation=windowsIn,1,5,easeOutBack,popin
+  animation=windowsOut,1,5,easeInBack,popin
+  animation=fadeIn,0
+  animation=fadeOut,1,10,easeInCubic
+  animation=workspaces,1,4,easeInOutCubic,slide
+  general:gaps_out=30
+  xwayland {
+    force_zero_scaling = true
+  }
+
+  monitor=HDMI-A-1,3840x2160,1920x0,2
+  monitor=eDP-1,2256x1504,0x0,1.5
+
+  input {
+    follow_mouse = 1
+      touchpad {
+        natural_scroll = true
+          disable_while_typing = true
+          tap-to-click = false
+          middle_button_emulation = false
+      }
+    sensitivity = 0
+  }
+  '';
+}
